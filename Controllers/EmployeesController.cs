ba78355e2c4ee7c28225069a7fe81446295f404a
MeowMeowJ <641911805@qq.com>
2020-03-17 19:39:26 +0800

    Lesson 13: Employee data and EmployeeDto

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Routine.Api.Entities;
+using Routine.Api.Models;
+using Routine.Api.Services;
+
+namespace Routine.Api.Controllers
+{
+    [ApiController]
+    [Route("api/companies/{companyId}/employees")]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly ICompanyRepository _companyRepository;
+        private readonly IMapper _mapper;
+
+        public EmployeesController(ICompanyRepository companyRepository, IMapper mapper)
+        {
+            _companyRepository = companyRepository ?? throw new ArgumentNullException(nameof(companyRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployeesForCompany(Guid companyId)
+        {
+            if (! await _companyRepository.CompanyExistsAsync(companyId))
+            {
+                return NotFound();
+            }
+
+            var employees = await _companyRepository.GetEmployeesAsync(companyId);
+
+            var employeeDtos = _mapper.Map<IEnumerable<EmployeeDto>>(employees);
+
+            return Ok(employeeDtos);
+        }
+    }
+}
