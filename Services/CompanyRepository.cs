4cd17046acdbdacc519cd6512886db916bf2e46c
MeowMeowJ <641911805@qq.com>
2020-03-17 23:33:35 +0800

    Lesson 19: CompanyDtoParameters

--- Services/CompanyRepository.cs
+++ Services/CompanyRepository.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using Microsoft.EntityFrameworkCore;
 using Routine.Api.Data;
+using Routine.Api.DtoParameters;
 using Routine.Api.Entities;
 
 namespace Routine.Api.Services
@@ -102,9 +103,32 @@ namespace Routine.Api.Services
             _context.Employees.Remove(employee);
         }
 
-        public async Task<IEnumerable<Company>> GetCompaniesAsync()
+        public async Task<IEnumerable<Company>> GetCompaniesAsync(CompanyDtoParameters parameters)
         {
-            return await _context.Companies.ToListAsync();
+            if (parameters == null)
+            {
+                throw new ArgumentNullException(nameof(parameters));
+            }
+
+            if (string.IsNullOrWhiteSpace(parameters.CompanyName) && string.IsNullOrWhiteSpace(parameters.SearchTerm))
+            {
+                return await _context.Companies.ToListAsync();
+            }
+
+            var queryExpression = _context.Companies as IQueryable<Company>;
+            if (!string.IsNullOrWhiteSpace(parameters.CompanyName))
+            {
+                parameters.CompanyName = parameters.CompanyName.Trim();
+                queryExpression = queryExpression.Where(x => x.Name == parameters.CompanyName);
+            }
+
+            if (!string.IsNullOrWhiteSpace(parameters.SearchTerm))
+            {
+                parameters.SearchTerm = parameters.SearchTerm.Trim();
+                queryExpression = queryExpression.Where(x =>
+                    x.Name.Contains(parameters.SearchTerm) || x.Introduction.Contains(parameters.SearchTerm));
+            }
+            return await queryExpression.ToListAsync();
         }
 
         public async Task<Company> GetCompanyAsync(Guid companyId)
