89d81517cfe73917e77c3f34250c88e494563fac
MeowMeowJ <641911805@qq.com>
2020-03-18 19:54:45 +0800

    Lesson 27 IValidatableObject

--- Models/EmployeeAddDto.cs
+++ Models/EmployeeAddDto.cs
@@ -1,14 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using Routine.Api.Entities;
 
 namespace Routine.Api.Models
 {
-    public class EmployeeAddDto
+    public class EmployeeAddDto: IValidatableObject
     {
+        [Display(Name = "员工号")]
+        [Required(ErrorMessage = "{0}是必填项")]
+        [StringLength(10, MinimumLength = 10, ErrorMessage = "{0}的长度是{1}")]
         public string EmployeeNo { get; set; }
+
+        [Display(Name = "名")]
+        [Required(ErrorMessage = "{0}是必填项")]
+        [MaxLength(50, ErrorMessage = "{0}的长度不能超过{1}")]
         public string FirstName { get; set; }
+
+        [Display(Name = "姓"), Required(ErrorMessage = "{0}是必填项"), MaxLength(50, ErrorMessage = "{0}的长度不能超过{1}")]
         public string LastName { get; set; }
+
+        [Display(Name = "性别")]
         public Gender Gender { get; set; }
+
+        [Display(Name = "出生日期")]
         public DateTime DateOfBirth { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (FirstName == LastName)
+            {
+                yield return new ValidationResult("姓和名不能相同", new [] {nameof(FirstName), nameof(LastName)});
+            }
+        }
     }
 }
