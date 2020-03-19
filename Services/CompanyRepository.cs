a75d973299611399dae52dce45d6e2e406a362a2
MeowMeowJ <641911805@qq.com>
2020-03-19 15:47:50 +0800

    Lesson 35: PreviousPageLink and NextPageLink

--- Services/CompanyRepository.cs
+++ Services/CompanyRepository.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Routine.Api.Data;
 using Routine.Api.DtoParameters;
 using Routine.Api.Entities;
+using Routine.Api.Helpers;
 
 namespace Routine.Api.Services
 {
@@ -103,7 +104,7 @@ namespace Routine.Api.Services
             _context.Employees.Remove(employee);
         }
 
-        public async Task<IEnumerable<Company>> GetCompaniesAsync(CompanyDtoParameters parameters)
+        public async Task<PagedList<Company>> GetCompaniesAsync(CompanyDtoParameters parameters)
         {
             if (parameters == null)
             {
@@ -124,10 +125,7 @@ namespace Routine.Api.Services
                     x.Name.Contains(parameters.SearchTerm) || x.Introduction.Contains(parameters.SearchTerm));
             }
 
-            queryExpression = queryExpression.Skip(parameters.PageSize * (parameters.PageNumber - 1))
-                .Take(parameters.PageSize);
-
-            return await queryExpression.ToListAsync();
+            return await PagedList<Company>.CreateAsync(queryExpression, parameters.PageNumber, parameters.PageSize);
         }
 
         public async Task<Company> GetCompanyAsync(Guid companyId)
